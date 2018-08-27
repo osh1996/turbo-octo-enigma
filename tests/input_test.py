cea3eed86df902bcd0b455a577ca3bf793eea133
Chris <osh1996@gmail.com>
2018-08-27 14:45:09 -0400

    deleted extraneous files

--- tests/input_test.py
+++
@@ -1,4 +0,0 @@
-name = input("What's your name? ")
-print("Nice to meet you " + str(name) + "!")
-age = input("Your age? ")
-print("So, you are already " + str(age) + " years old, " + str(name) + "!")
